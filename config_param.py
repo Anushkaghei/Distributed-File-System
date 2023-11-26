736594d673dff2643a88d7be2c4d8f546d84eecf
Anushka Ghei <79694271+Anushkaghei@users.noreply.github.com>
2023-11-27 03:52:05 +0530

    Update config_param.py

--- config_param.py
+++ config_param.py
@@ -1,15 +1,15 @@
 # Name node details
-name_node_host = "172.20.10.2"
-name_node_port = 5000
+name_node_host = " "
+name_node_port = 
 name_node_url = "http://" + name_node_host + ":" + str(name_node_port)
 
 # Data node details
-data_node_host = "172.20.10.3"
-data_node_port = 8000
+data_node_host = " "
+data_node_port = 
 
 # Define the directory for metadata
-metadata_dir = r'C:\Users\HP\Desktop\YADFS\RR-Team-9-Yet-Another-Distributed-File-System-YADFS-\metadata'
+metadata_dir = r' '
 metadata_file_name = "metadata.json"
 
 #define the directory for data blocks
-data_blocks_dir = r'C:\Users\HP\Desktop\YADFS\RR-Team-9-Yet-Another-Distributed-File-System-YADFS-\datablocks'
+data_blocks_dir = r' '
