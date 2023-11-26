fa08714744818fca71a8a51a842329e7157d90df
Anushka Ghei <79694271+Anushkaghei@users.noreply.github.com>
2023-11-26 14:07:28 +0530

    Add files via upload

---
+++ config_param.py
@@ -0,0 +1,15 @@
+# Name node details
+name_node_host = "172.20.10.2"
+name_node_port = 5000
+name_node_url = "http://" + name_node_host + ":" + str(name_node_port)
+
+# Data node details
+data_node_host = "172.20.10.3"
+data_node_port = 8000
+
+# Define the directory for metadata
+metadata_dir = r'C:\Users\HP\Desktop\YADFS\RR-Team-9-Yet-Another-Distributed-File-System-YADFS-\metadata'
+metadata_file_name = "metadata.json"
+
+#define the directory for data blocks
+data_blocks_dir = r'C:\Users\HP\Desktop\YADFS\RR-Team-9-Yet-Another-Distributed-File-System-YADFS-\datablocks'
